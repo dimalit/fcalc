4bc5aab781f3bb91075a3962f16ba5940f7d2435
Dima Litvinov <dima.litvinov@skalelabs.com>
2025-12-30 23:19:45 +0000

    Added all for PWA

--- Expo/webpack.config.js
+++ Expo/webpack.config.js
@@ -1,10 +1,20 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
 
-  if( env.mode === 'production')
+  if( env.mode === 'production'){
     config.output.publicPath = '/fcalc/'; 
+    config.plugins.push(
+      new WorkboxPlugin.GenerateSW({
+        clientsClaim: true,
+        skipWaiting: true,
+        // optional tweaks:
+        // navigateFallback: '/index.html',
+      })
+    );
+  }// if
 
   return config;
 };
